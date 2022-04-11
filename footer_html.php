4fd0c9dd118f149b33a7d111496a1413f83633db
opurushothaman <omprakash.purushothaman@rntbci.com>
2022-04-11 19:34:57 +0530

    remove website carbon badge

--- footer_html.php
+++ footer_html.php
@@ -1,8 +1,8 @@
 
 <div class="container-fluid">
                      <div class="footer">
-                     <div id="wcb" class="carbonbadge wcb-d"></div>
-                        <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script>
+                     <!-- <div id="wcb" class="carbonbadge wcb-d"></div>
+                        <script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script> -->
                         <p>Copyright © 2018 Designed by html.design. All rights reserved.</p>
                      </div>
                   </div>
